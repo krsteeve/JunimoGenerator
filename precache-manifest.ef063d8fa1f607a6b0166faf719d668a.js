6d81b33bacff72db21858b1e399f6da95f35de2d
Kelsey Steeves <kelsey.steeves@youi.tv>
2019-11-06 15:32:33 -0500

    Updates

---
+++ precache-manifest.ef063d8fa1f607a6b0166faf719d668a.js
@@ -0,0 +1,57 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794563da9e40624ff13079720d62eace",
+    "url": "/JunimoGenerator/index.html"
+  },
+  {
+    "revision": "5c3ec6951dfbfe2a37a9",
+    "url": "/JunimoGenerator/static/css/2.e264405f.chunk.css"
+  },
+  {
+    "revision": "e5f64233051d5e4b9c1d",
+    "url": "/JunimoGenerator/static/css/main.5c233135.chunk.css"
+  },
+  {
+    "revision": "5c3ec6951dfbfe2a37a9",
+    "url": "/JunimoGenerator/static/js/2.f986eb47.chunk.js"
+  },
+  {
+    "revision": "e5f64233051d5e4b9c1d",
+    "url": "/JunimoGenerator/static/js/main.0ea6534e.chunk.js"
+  },
+  {
+    "revision": "1a1ffe5bb012211fb262",
+    "url": "/JunimoGenerator/static/js/runtime~main.a833dd79.js"
+  },
+  {
+    "revision": "75c01ef71b274d57cd56983ebdfebbe4",
+    "url": "/JunimoGenerator/static/media/art-artistic-background-1851447.75c01ef7.jpg"
+  },
+  {
+    "revision": "3bc89dfed3578554ab5d4174a52f1e1d",
+    "url": "/JunimoGenerator/static/media/art-beautiful-bloom-1037994.3bc89dfe.jpg"
+  },
+  {
+    "revision": "2e6af9f878e96f8b3d5c7d6037b01944",
+    "url": "/JunimoGenerator/static/media/background-beautiful-beauty-2033997.2e6af9f8.jpg"
+  },
+  {
+    "revision": "adec247b8715abe37fa37a4ce1126ba9",
+    "url": "/JunimoGenerator/static/media/background-beautiful-bloom-414667.adec247b.jpg"
+  },
+  {
+    "revision": "189caddaf28edd853d2673d8c4374129",
+    "url": "/JunimoGenerator/static/media/background-biology-blue-1426718.189cadda.jpg"
+  },
+  {
+    "revision": "1ac2b713cc2143f5e4740263dde24509",
+    "url": "/JunimoGenerator/static/media/blur-bright-color-751374.1ac2b713.jpg"
+  },
+  {
+    "url": "/JunimoGenerator/static/media/checkbcd1a436590360ed53ac2f46c76e43e4.bcd1a436.png"
+  },
+  {
+    "revision": "4c22bdfbdbf02a96fa4fe76561c27666",
+    "url": "/JunimoGenerator/static/media/color-conifer-daylight-1179229.4c22bdfb.jpg"
+  }
+]);
